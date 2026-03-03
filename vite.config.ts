6d12ca29f8f5c84559f7eb857138e8ed723bd3a8
DulanjanaLakshan <dulanjana20013@gmail.com>
2026-03-03 15:25:14 +0530

    update

--- vite.config.ts
+++ vite.config.ts
@@ -10,6 +10,11 @@ export default defineConfig(({mode}) => {
     define: {
       'process.env.GEMINI_API_KEY': JSON.stringify(env.GEMINI_API_KEY),
     },
+    preview: {
+      host: true,
+      port: 3000,
+      allowedHosts: ["drug-cosmetic-toiletry-wholesaling.cypsolabs.com","www.drug-cosmetic-toiletry-wholesaling.cypsolabs.com"],
+    },
     resolve: {
       alias: {
         '@': path.resolve(__dirname, './src'),
